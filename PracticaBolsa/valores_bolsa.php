cacfd00141f7172900f450a34a8fef63fd63b79d
JhojaniSilvestre <90837625+JhojaniSilvestre@users.noreply.github.com>
2021-10-06 21:11:01 +0200

    Update valores_bolsa.php

--- PracticaBolsa/valores_bolsa.php
+++ PracticaBolsa/valores_bolsa.php
@@ -1,1 +1,22 @@
+<?php
+/*Jhojani Silvestre Beltrán */
 
+//$empresa = array(array()); 
+$nombreEmp = "empresa";
+
+for ($i=0; $i < 35; $i++) { //Relleno el array 35 x 9
+    $datoRandom= rand(10, 30);
+    $empresa[$nombreEmp.($i+1)] = array("indice1"=>$datoRandom, "indice2"=>$datoRandom, "indice3" => $datoRandom,
+     "indice4" => $datoRandom, "indice5" => $datoRandom, "indice6" => $datoRandom, "indice7" => $datoRandom
+     , "indice8" => $datoRandom);   
+}
+
+foreach ($empresa as $nombreEmp => $valor) { //imprimo el array
+    print "$nombreEmp | ";
+    foreach ($valor as $key => $value) {
+        print "$key: $value | ";
+    }
+    print "<br />";
+}
+
+?>
