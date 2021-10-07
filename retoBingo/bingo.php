b588a66c01df12ee0417cbaeecbc0d4e2b2d565e
JhojaniSilvestre <90837625+JhojaniSilvestre@users.noreply.github.com>
2021-10-07 19:35:27 +0200

    Update bingo.php

--- retoBingo/bingo.php
+++ retoBingo/bingo.php
@@ -1,1 +1,33 @@
+<?php
+$carton = array();
+$numRandAux = array();
+$i=0;
+do{
+    $numRand=rand(1,60);
+    if (!in_array($numRand, $numRandAux)) {
+        $numRandAux[$i] = $numRand;
+        $carton[$i] = $numRand; 
+        $i++;
+    }
+}while(count($carton) < 15);
 
+print_r($carton);
+
+$aciertos=0;
+$bomboAux= array();
+$j=0;
+while ($aciertos < 15) {
+    $bombo=rand(1,60);
+    if(!in_array($bombo, $bomboAux)){
+        $bomboAux[$j] = $bombo;
+        if (in_array($bombo, $carton)) {
+            $aciertos++;
+            echo "<br/>$bombo";
+        }
+        $j++;
+    }
+}
+echo "<br/> ";
+print_r($bomboAux);
+
+?>
