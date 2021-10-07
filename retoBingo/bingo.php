e4f91e7044e6a396b2075517848df7f0911cf394
JhojaniSilvestre <90837625+JhojaniSilvestre@users.noreply.github.com>
2021-10-07 20:08:46 +0200

    Update bingo.php

--- retoBingo/bingo.php
+++ retoBingo/bingo.php
@@ -1,30 +1,28 @@
 <?php
 $carton = array();
-$numRandAux = array();
 $i=0;
 do{
     $numRand=rand(1,60);
-    if (!in_array($numRand, $numRandAux)) {
-        $numRandAux[$i] = $numRand;
-        $carton[$i] = $numRand; 
-        $i++;
+    if (!in_array($numRand, $carton)) { //si el num random no se encuentra en el carton:
+        $carton[$i] = $numRand; //guardo el numero en el carton
+        $i++; //incremento la posicion
     }
 }while(count($carton) < 15);
 
 print_r($carton);
 
 $aciertos=0;
-$bomboAux= array();
+$bomboAux= array(); //guardará los numeros aleatorios del bombo sin repetir
 $j=0;
 while ($aciertos < 15) {
     $bombo=rand(1,60);
-    if(!in_array($bombo, $bomboAux)){
-        $bomboAux[$j] = $bombo;
-        if (in_array($bombo, $carton)) {
-            $aciertos++;
+    if(!in_array($bombo, $bomboAux)){ //si el numero aleatorio del bombo no se encuentra en bomboAux:
+        $bomboAux[$j] = $bombo;//guardo el numero en el array bomboAux
+        if (in_array($bombo, $carton)) { //si el num aleatorio del bombo se encuentra en el carton:
+            $aciertos++; //incremento los aciertos
             echo "<br/>$bombo";
         }
-        $j++;
+        $j++;//incremento la posicion
     }
 }
 echo "<br/> ";
