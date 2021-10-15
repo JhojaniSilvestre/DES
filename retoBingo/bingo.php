36fc4d15e71174d992535ae3c4098b53cf9dfb27
JhojaniSilvestre <90837625+JhojaniSilvestre@users.noreply.github.com>
2021-10-15 13:57:11 +0200

    Update bingo.php

--- retoBingo/bingo.php
+++ retoBingo/bingo.php
@@ -1,17 +1,16 @@
-
 <?php
 
 //relleno el array jugadores 
 $cont=0;
 $carton = array();
-for ($i=0; $i < 12; $i++) {//total de cartones por cada jugador, en este caso 4 jugadores cada uno con 3 cartones
+for ($i=0; $i < 12; $i++) { //total de cartones por cada jugador, en este caso 4 jugadores cada uno con 3 cartones
     $j=0;
     $k=1;
-    if ($i % 3 == 0) {//esto controla cuantos cartones tiene un jugador
+    if ($i % 3 == 0) { //esto controla cuantos cartones tiene un jugador
         $jugadores[$i][$j] = "jugador".$cont++;
     }
     $jugadores[$i][$j] = "jugador".$cont;
-    
+
     do{
         $numRand=rand(1,60);
         if (!in_array($numRand, $carton)) {
@@ -23,6 +22,42 @@ for ($i=0; $i < 12; $i++) {//total de cartones por cada jugador, en este caso 4 
     $carton = array();
 }
 var_dump($jugadores);
+echo "<br>";
+
+//otra forma de visualizar en cuadrado
+
+for ($i = 0; $i < sizeof($jugadores); $i++) {
+    for ($j = 0; $j < sizeof($jugadores[0]); $j++){
+        echo $jugadores[$i][$j]."\n";
+    }
+    echo "<br>";
+}
+
+
+$bomboAux= array();
+$jugadorWin;
+$aciertos = 0;
+$b=0;
+while ($aciertos < 15) {
+    $bombo=rand(1,60);
+    if(!in_array($bombo, $bomboAux)){
+        $bomboAux[$b] = $bombo;
+        for ($i = 0; $i < sizeof($jugadores); $i++) {
+            for ($j = 0; $j < sizeof($jugadores[0]); $j++){
+                if($jugadores[$i][$j] == $bomboAux[$b]){
+                    $aciertos++;
+                    $jugadorWin = $jugadores[$i][0];
+                    echo "<br/> bola: $bombo  "; //
+                    echo " ".$jugadorWin;
+                }
+            }
+        }
+        $b++;
+    }
+}
+echo "<br>";
+
+
 
 /*
 $aciertos=0;
