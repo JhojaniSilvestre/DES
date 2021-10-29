dbcfee5c774fb9ab220161d72b7e4640f50062d9
JhojaniSilvestre <90837625+JhojaniSilvestre@users.noreply.github.com>
2021-10-29 09:32:55 +0200

    Add files via upload

---
+++ files/ejer1/funcion_fichero1.php
@@ -0,0 +1,22 @@
+<?php
+
+function validarFecha($fecha){
+    $valores = explode("/",$fecha);
+    if (count($valores) == 3 && checkdate($valores[1],$valores[0],$valores[2])) {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function leerFichero($fichero){
+    $fileLeer = fopen($fichero, "r") or die("No es posible abrir el fichero");
+
+    while(!feof($fileLeer)) {
+        echo fgets($fileLeer) . "<br>";
+    }
+    fclose($fileLeer);
+}
+
+?>
