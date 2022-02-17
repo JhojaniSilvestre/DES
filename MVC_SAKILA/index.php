db9b763dcd86bf4a2565425cc9d50e18225364c0
JhojaniSilvestre <90837625+JhojaniSilvestre@users.noreply.github.com>
2022-02-17 08:58:08 +0100

    Delete MVC_SAKILA directory

--- MVC_SAKILA/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "Inicio index"."<br>";
-// Llamada al fichero que inicia la conexión a la Base de Datos
-require_once("db/conexion.php");
-
-// Llamada al controlador
-require_once("controllers/login_controller.php");
-echo "Fin index"."<br>";
-?>
